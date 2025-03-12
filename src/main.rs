040207a5d274a44bf544cd4c628f24646f609700
jkdzzzz <pascal.pri0502@gmail.com>
2025-03-13 00:06:24 +0100

    first basic calculation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
-    println!("Simple Rust Calculator | CLI");
     let mut first_num = String::new();
+    let mut second_num = String::new();
+
+    println!("Simple Rust Calculator | CLI");
+
+    print!("Please input the first number: ");
+    io::stdout().flush().expect("Failed to flush stdout!");
     io::stdin()
     .read_line(&mut first_num)
     .expect("Not a valid input!");
-
     let first_num: f32 = first_num.trim().parse().expect("Input is not a valid number!");
 
-    
+    print!("Please input the second number: ");
+    io::stdout().flush().expect("Failed to flush stdout!");
+    io::stdin()
+    .read_line(&mut second_num)
+    .expect("Not a valid input!");
+    let second_num: f32 = second_num.trim().parse().expect("Input is not a valid number!");
+
+    println!("{}", first_num + second_num);
 }
